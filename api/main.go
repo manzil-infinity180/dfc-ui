7d0cd29eb387007a3d96c2a981872ea8b791fec4
Rahul Vishwakarma <rahulvs2809@gmail.com>
2025-08-04 02:10:13 +0530

    wip: added simple conversion from Dockerfile file to Chainguard

---
+++ api/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/chainguard-dev/dfc/pkg/dfc"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	// raw = []byte(strings.TrimSpace(`
+	// 	FROM node
+	// 	RUN apt-get update && apt-get install -y nano
+	// `))
+	org = "example.com"
+)
+
+func main() {
+	router := gin.Default()
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+	// demoConversion()
+	router.POST("/upload", readDockerfileFromUpload)
+	router.Run(":8000")
+}
+
+func readDockerfileFromUpload(ctx *gin.Context) {
+	req := ctx.Request
+	file, handler, err := req.FormFile("dockerfile")
+	if err != nil {
+		ctx.JSON(http.StatusNotAcceptable, gin.H{"error": err.Error()})
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Received file: %s\n", handler.Filename)
+	// Read file contents
+	content, err := io.ReadAll(file)
+	if err != nil {
+		ctx.JSON(http.StatusNotAcceptable, gin.H{"error": err.Error()})
+		return
+	}
+	fmt.Println("=== Dockerfile Content ===")
+	fmt.Println(string(content))
+	fmt.Println("=== Dockerfile After Content ===")
+	new_dockerfile := demoConversion(string(content))
+
+	fmt.Println(new_dockerfile)
+	ctx.JSON(http.StatusAccepted, gin.H{
+		"status":         "ok",
+		"old_dockerfile": string(content),
+		"new_dockerfile": new_dockerfile.String(),
+	})
+
+}
+
+func demoConversion(dockerfile_raw string) *dfc.Dockerfile {
+	ctx := context.Background()
+	// Parse the Dockefile bytes
+	raw := []byte(strings.TrimSpace(dockerfile_raw))
+
+	dockerfile, err := dfc.ParseDockerfile(ctx, raw)
+	if err != nil {
+		log.Fatalf("ParseDockerfile(): %v", err)
+	}
+
+	// Convert
+	converted, err := dockerfile.Convert(ctx, dfc.Options{
+		Organization: org,
+		// Registry: "r.example.com/cgr-mirror", // Optional: registry override
+		// Update:   true,                      // Optional: update mappings before conversion
+		// ExtraMappings: myCustomMappings,     // Optional: overlay mappings on top of builtin
+		// NoBuiltIn: true,                     // Optional: skip built-in mappings
+	})
+	if err != nil {
+		log.Fatalf("dockerfile.Convert(): %v", err)
+	}
+
+	// Print converted Dockerfile content
+	fmt.Println(converted)
+	return converted
+}
